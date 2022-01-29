bb5351c7237981b9d2eb46cc7a6d4e46052d0136
Aditya Saxena <adity09@outlook.com>
2022-01-29 20:11:55 +0530

    CLI

--- mdb-cli/main.cpp
+++ mdb-cli/main.cpp
@@ -1,9 +1,55 @@
-#include <iostream>
+#include "cxxopts.h"
+#include "mdb/database.h"
+#include "mdb/mdb.h"
 
-using namespace std;
+cxxopts::Options opts("mdb-cli", "CLI for MaverickDB");
 
-int main()
-{
-    cout << "Hi there\n";
-    return 0;
+#define logUsage() std::cout<<"Invalid configuration!\n";
+
+int main(int argCnt, char*args[]){
+    opts.add_options()
+            ("c, create", "CREATE A DATABASE")
+            ("d, destroy", "DESTROY A DATABASE")
+            ("s, set","SET A KEY IN A DATABASE")
+            ("g, get","GET A KEY FROM A DATABASE")
+            ("n, name", "DATABASE NAME(MANDATORY)", cxxopts::value<std::string>())
+            ("k, key", "KEY TO SET/GET",cxxopts::value<std::string>())
+            ("v, value","VALUE TO SET", cxxopts::value<std::string>());
+    auto res=opts.parse(argCnt, args);
+    if(res.count("c")==1){
+        if(!res.count("n")){
+            std::cout<<"ERROR:\tMust provide database name with -n <name>\n";
+            logUsage()
+            return 1;
+        }
+        std::string dbName=res["n"].as<std::string>();
+        database db(Mdb::createFreshDB(dbName));
+        return 0;
+    }
+    if (res.count("s") == 1) {
+        if (!res.count("n")) {
+            std::cout << "ERROR:\tMust provide database name with -n <name>\n";
+            logUsage();
+            return 1;
+        }
+        if (!res.count("k")) {
+            std::cout << "ERROR:\tMust provide a key with -k <name>\n";
+            logUsage();
+            return 1;
+        }
+        if (!res.count("v")) {
+            std::cout << "ERROR:\tMust provide a value with -v <value>\n";
+            logUsage();
+            return 1;
+        }
+        std::string dbname=res["n"].as<std::string>(),
+                k(res["k"].as<std::string>()),
+                v(res["v"].as<std::string>());
+        database db(Mdb:loadDB(dbName));
+        db.setKeyValue(k,v);
+        return 0;
+    }
+    std::cout<<"ERROR:\tNo command provided!\n";
+    logUsage()
+    return 1;
 }
